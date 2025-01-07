6c691eae715ce65e17073a6c9cb59db5e44a6fd6
PhiHung151 <hungca1501@gmail.com>
2025-01-07 15:58:53 +0700

    update ListGroup

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,21 @@
+import { Fragment } from "react/jsx-runtime";
 function ListGroup() {
+    const items = [
+        'Viet Nam',
+        'Deutch',
+        'England'
+    ];
     return (
-        <ul className="list-group">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
-        </ul>
+        <Fragment>
+            <div>
+                <h1>List</h1> 
+                <ul className="list-group">
+                    {items.map(item => (
+                        <li>{item}</li>
+                    ))}
+                </ul>
+            </div>
+        </Fragment>
     );
 }
 export default ListGroup;
